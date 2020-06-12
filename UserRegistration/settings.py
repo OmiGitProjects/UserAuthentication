5ab11ca44cbfcc3af978c540b66821c5b835ee0f
OmiGitProjects <onkarnardekar2407@gmail.com>
2020-06-12 23:44:31 +0530

    Media Root Error Fixed

--- UserRegistration/settings.py
+++ UserRegistration/settings.py
@@ -132,7 +132,7 @@ STATICFILES_DIRS = [
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/media/')
 
 
 # Form Styling and Version Changed
