ee8cbe8932f0025b19e54f2bfead5289eb031ded
OmiGitProjects <onkarnardekar2407@gmail.com>
2020-06-12 22:40:46 +0530

    Media Root

--- UserRegistration/urls.py
+++ UserRegistration/urls.py
@@ -27,4 +27,5 @@ urlpatterns = [
         path('logout/', auth_views.LogoutView.as_view(template_name="userApp/logout.html"), name="Logout"),
         path('', include('userBlog.urls')),
 
-] +  static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
