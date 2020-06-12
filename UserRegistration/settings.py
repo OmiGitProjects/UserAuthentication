74eab970e4c11d400779e83f93619997d83e9485
OmiGitProjects <onkarnardekar2407@gmail.com>
2020-06-12 22:33:43 +0530

    Media URL

--- UserRegistration/settings.py
+++ UserRegistration/settings.py
@@ -131,8 +131,8 @@ STATICFILES_DIRS = [
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 # Form Styling and Version Changed
